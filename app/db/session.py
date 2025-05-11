0d64b73fc5b74cd8283f898ec90b4e76d875ff80
Bilal31313 <bilalkhawaja04@outlook.com>
2025-05-11 22:25:39 +0000

    Fix: define get_session and package db module

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,5 @@
+from sqlmodel import create_engine, Session
 import os
-from sqlmodel import SQLModel, create_engine
 
 DB_USER = os.getenv("DB_USERNAME")
 DB_PASS = os.getenv("DB_PASSWORD")
@@ -7,4 +7,8 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = "costs"
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
+
 engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    return Session(engine)
